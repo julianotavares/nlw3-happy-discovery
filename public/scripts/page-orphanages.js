5ae85f4c77cc5d1a6eee13d60b25926b152194ef
Juliano Tavares <juliano.tavaress@gmail.com>
2020-11-09 21:32:05 -0300

    Added orphanage.html

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -19,7 +19,7 @@ const popup = L.popup({
   className: 'map-popup',
   minWidth: 240,
   minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage" <img src="./public/images/arrow-white.svg" > </a>')
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg" > </a>')
 
 L.marker([-17.5674154,-52.5546287], { icon })
 .addTo(map)
